125e62aceef3636b1147e1d3598a81e1a0044b8c
giacomo <thegreat.vanderlinde@gmail.com>
2022-12-16 19:36:34 +0100

    fix font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import Head from 'next/head';
 
 import CallToAction from '../components/CallToAction';
@@ -11,6 +12,9 @@ export default function Home() {
 				<title>byte3 | digital solutions for growth business</title>
 				<meta name="description" content="byte3 | digital solutions for growth business" />
 				<link rel="icon" href="/favicon.ico" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
 			</Head>
 			<Hero />
 			<FeatureComponent />
